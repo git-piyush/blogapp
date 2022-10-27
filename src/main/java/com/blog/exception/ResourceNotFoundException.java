0b6698775a6f6ad60ee6826d6ffd9608e92ad2fc
Piyush kumar <kmrpiyush95@gmail.com.com>
2022-10-27 11:49:34 +0530

    40. Creating Custom Exception - ResourceNotFoundException

---
+++ src/main/java/com/blog/exception/ResourceNotFoundException.java
@@ -0,0 +1,71 @@
+package com.blog.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String resourceName;
+	
+	private String fieldName;
+	
+	private String fieldValue;
+
+	public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+		super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+		this.resourceName = resourceName;
+		this.fieldName = fieldName;
+		this.fieldValue = fieldValue;
+	}
+
+	/**
+	 * @return the resourceName
+	 */
+	public String getResourceName() {
+		return resourceName;
+	}
+
+	/**
+	 * @param resourceName the resourceName to set
+	 */
+	public void setResourceName(String resourceName) {
+		this.resourceName = resourceName;
+	}
+
+	/**
+	 * @return the fieldName
+	 */
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	/**
+	 * @param fieldName the fieldName to set
+	 */
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	/**
+	 * @return the fieldValue
+	 */
+	public String getFieldValue() {
+		return fieldValue;
+	}
+
+	/**
+	 * @param fieldValue the fieldValue to set
+	 */
+	public void setFieldValue(String fieldValue) {
+		this.fieldValue = fieldValue;
+	}
+	
+	
+
+}
