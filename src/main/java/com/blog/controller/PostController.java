0b4bb91fbd471b594fcbef47c465a9622fc39bca
Piyush kumar <kmrpiyush95@gmail.com.com>
2022-10-28 22:44:18 +0530

    Securing REST API's with In-memory Authentication

--- src/main/java/com/blog/controller/PostController.java
+++ src/main/java/com/blog/controller/PostController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ public class PostController {
 	@Autowired
 	PostService postService;
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping("/post")
 	public ResponseEntity<PostDTO> createPost(@Valid @RequestBody PostDTO postDTO) {
 
@@ -55,6 +57,7 @@ public class PostController {
 
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@PutMapping("/post/{postId}")
 	public ResponseEntity<PostDTO> updatePostByPostId(@Valid @RequestBody PostDTO postDTO,
 			@PathVariable(name = "postId") Long postId) {
@@ -65,6 +68,7 @@ public class PostController {
 
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@DeleteMapping("/post/{postId}")
 	public ResponseEntity<String> deletePostByPostId(@PathVariable(name = "postId") Long postId) {
 
